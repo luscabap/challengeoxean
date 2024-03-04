7ea5f53286163905e23dfd7d2e7e9f613e34f2a6
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-03-04 12:41:52 -0300

    Colunas com informações filtráveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import './App.scss';
-import { Grid, GridColumn } from '@progress/kendo-react-grid';
-import { DropDownList } from "@progress/kendo-react-dropdowns";
-import products from "./products.json";
-import categories from "./categories.json";
+import { Grid, GridColumn as Column, GridFilterChangeEvent } from '@progress/kendo-react-grid';
+import { useEffect, useState } from 'react';
+import { filterBy, CompositeFilterDescriptor } from '@progress/kendo-data-query';
+import axios, { AxiosResponse  } from 'axios'
+import { IPropsReq } from './interface/IPropsRequisicao';
+
+const initialFilter: CompositeFilterDescriptor = {
+  logic: "and",
+  filters: [{ field: "episode", operator: "contains", value: "" }]
+};
 
 function App() {
+  const [filter, setFilter] = useState(initialFilter);
+  
+  const [resultado, setResultado] = useState<IPropsReq[]>([])
+
+    useEffect(() => {
+    const reqRickMorty = async () => {
+      try {
+        const response = await axios.get('https://rickandmortyapi.com/api/episode');
+        const resultado = response.data.results;
+        setResultado(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    reqRickMorty();
+  }, []);
 
   return (
     <div className="App">
-      <p>
-        <DropDownList 
-          data={categories}
-          dataItemKey="CategoryID"
-          textField="CategoryName"
+      <Grid 
+        data={filterBy(resultado, filter)}
+        filterable={true}
+        filter={filter}
+        onFilterChange={(e: GridFilterChangeEvent) => setFilter(e.filter)}
+        style={{ backgroundColor:"#f0F" }}
+      >
+        <Column field="id" title="ID" width="200px" />
+        <Column field="episode" title="Episódio" width="240px" />
+        <Column
+          field="air_date"
+          width="240px"
+          filter="date"
+          format="{0:Y}"
+          title='Data'
         />
-      </p>
-      <Grid data={products}>
-        <GridColumn field="ProductName" title="Product Name" format="{0:c}"/>
-        <GridColumn field="UnitPrice" />
-        <GridColumn field="UnitsInStock" />
-        <GridColumn field="Discontinued" />
+        <Column field="name" title="Nome do episódio" width="180px" format="{0:c}" />
       </Grid>
     </div>
   );
---
+++ src/interface/IPropsRequisicao.ts
@@ -0,0 +1,8 @@
+export interface IPropsReq{
+    air_date: string,
+    episode: string,
+    id: number,
+    name: string,
+    url: string,
+    characters: string[] 
+  }
